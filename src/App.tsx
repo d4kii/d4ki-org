32d537e0e214bf0e14cf03b957c77a6e77f11398
dakimasuSchool <166367193+dakimasuSchool@users.noreply.github.com>
2024-11-07 09:12:56 +0000

    Sum Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./Layout";
 import NoMatch from "./NoMatch";
+import Home from "./Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="*" element={<NoMatch />} />
+        <Route path="/" element={<Home />} />
       </Route>
     </Routes>
   );
---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div id="Home">
+
+    </div>
+  );
+}
+
+export default Home;
